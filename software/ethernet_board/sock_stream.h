02439f2a56e22822d3a247e29f935acda619bbc4
wenwen <wenwen@wwad.de>
2014-06-14 16:17:06 +0200

    sock stream implement

---
+++ software/ethernet_board/sock_stream.h
@@ -0,0 +1,19 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+#ifndef SOCK_STREAM_H
+#define SOCK_STREAM_H
+#include <avr/io.h>
+#include <stdio.h>
+
+extern FILE sock_stream;
+#define MAX_RESPONSE_LEN 5
+
+void sock_stream_init(void);
+int sock_stream_flush();
+void stream_set_sock(uint8_t sock);
+static int sock_putchar(char c, FILE *stream);
+#endif
+#ifdef __cplusplus
+}
+#endif
